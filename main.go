0a5d0612cb6498b2023860e573d0fe68645a5155
starfork <tarsier.t99@gmail.com>
2024-08-18 11:37:47 +0000

    bug fix

--- main.go
+++ main.go
@@ -128,7 +128,7 @@ func ReadFromTxt(path string) (map[string][]string, error) {
 			if len(tldArr) > 0 && !inArray(tldArr, url.TLD) {
 				continue
 			}
-			if len(subArr) > 0 && !inArray(tldArr, url.Subdomain) {
+			if url.Subdomain != "" && len(subArr) > 0 && !inArray(subArr, url.Subdomain) {
 				continue
 			}
 
